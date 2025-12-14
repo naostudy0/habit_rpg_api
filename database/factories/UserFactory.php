03b7f89e653bb933c2f3774473153c42008121d4
nao.u <nao.study0@gmail.com>
2025-12-14 23:12:55 +0900

    feat: データベース設計・作成（認証） #5

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
@@ -27,7 +26,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'password',
+            'is_dark_mode' => false,
+            'is_24_hour_format' => true,
             'remember_token' => Str::random(10),
         ];
     }
@@ -39,6 +40,26 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * ダークモードを有効にする
+     */
+    public function darkMode(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_dark_mode' => true,
+        ]);
+    }
+
+    /**
+     * 12時間形式に設定する
+     */
+    public function twelveHourFormat(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_24_hour_format' => false,
         ]);
     }
 }
